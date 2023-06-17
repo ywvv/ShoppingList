0684cb146e8418bb1dc9bb4bd9f7525422a85148
Vlad Horban <ywvvvw@gmail.com>
2023-06-17 13:27:06 +0300

    Add ShopListRepository

--- app/src/main/java/com/example/shoppinglist/domain/DeleteShopItemUseCase.kt
+++ app/src/main/java/com/example/shoppinglist/domain/DeleteShopItemUseCase.kt
@@ -1,8 +1,8 @@
 package com.example.shoppinglist.domain
 
-class DeleteShopItemUseCase {
+class DeleteShopItemUseCase(private val shopListRepository: ShopListRepository) {
 
     fun deleteShopItem(shopItem: ShopItem) {
-
+        shopListRepository.deleteShopItem(shopItem)
     }
 }
